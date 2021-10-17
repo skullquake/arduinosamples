f9d82edd2a708a61e71e0571be3a9e81a280f4c4
skullquake <ockert8080@gmail.com>
2021-10-17 22:49:21 +0200

    .

--- src/c_cpp/src/main.cpp
+++ src/c_cpp/src/main.cpp
@@ -1,36 +1,26 @@
-#include <Arduino.h>
-int atexit(void (* /*func*/ )()) { return 0; }
-// Weak empty variant initialization function.
-// May be redefined by variant files.
-void initVariant() __attribute__((weak));
-void initVariant() { }
-
-void setupUSB() __attribute__((weak));
-void setupUSB() { }
-
+#include<Arduino.h>
+int atexit(void(* /*func*/)()){return 0;}
+void initVariant()__attribute__((weak));
+void initVariant(){}
+void setupUSB()__attribute__((weak));
+void setupUSB(){}
 void setup(){
 	//setup as in ino
 }
 void loop(){
 	//loop as in ino
 }
-int main(void)
-{
+int main(void){
 	init();
-
 	initVariant();
-
 #if defined(USBCON)
 	USBDevice.attach();
 #endif
-	
 	setup();
-    
-	for (;;) {
+	for(;;){
 		loop();
-		if (serialEventRun) serialEventRun();
+		if(serialEventRun)serialEventRun();
 	}
-        
 	return 0;
 }
 
